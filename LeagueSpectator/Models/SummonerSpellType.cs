33a0e36ea9b0bf3b378eca8423d2f876f4d90894
arturitus <arturogranadosgimeno@gmail.com>
2022-11-03 14:49:22 +0100

    añadir summ spell champs

---
+++ LeagueSpectator/Models/SummonerSpellType.cs
@@ -0,0 +1,27 @@
+﻿namespace LeagueSpectator.Models
+{
+    public enum SummonerSpellType : short
+    {
+        None = 54,
+        Cleanse = 1,
+        Exhaust = 3,
+        Flash = 4,
+        WierdMark = 5,
+        Ghost = 6,
+        Heal = 7,
+        Smite = 11,
+        Teleport = 12,
+        Clarity = 13,
+        Ignite = 14,
+        Barrier = 21,
+        PoroKingCookie = 30,
+        PoroKingMark = 31,
+        Mark = 32,
+        Mark_1 = 39,
+        AscensionRevive = 50,
+        Ghost_1 = 51,
+        Dash = 52,
+        RuinedKingSmite = 55,
+
+    }
+}
