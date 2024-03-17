9185784612f1e8f2d06e6e58838bea4850b557dc
arturitus <arturogranadosgimeno@gmail.com>
2024-03-17 14:03:15 +0100

    Update getAccountByNameTag.js

--- functions/getAccountByNameTag.js
+++ functions/getAccountByNameTag.js
@@ -2,25 +2,26 @@ const axios = require('axios');
 
 // Function to retrieve summoner by name
 exports.handler = async (event, context) => {
-  try {
+//   try {
     const { riotServerRegion, summonerName, tagLine } = event.queryStringParameters;
     const apiKey = process.env.API_KEY; // Accessing the API key from environment variable
     const url = `https://${riotServerRegion}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tagLine}?api_key=${apiKey}`;
     
+    return url
     // Fetch data from the API
     const response = await axios.get(url);
 
     // Return the response to the client
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response.data)
-    };
-  } catch (error) {
-    // If an error occurs, return an error response
-    console.error(`Error fetching data: ${error.message}`);
-    return {
-      statusCode: error.response.status || 500,
-      body: JSON.stringify({ error: error.message })
-    };
-  }
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(response.data)
+//     };
+//   } catch (error) {
+//     // If an error occurs, return an error response
+//     console.error(`Error fetching data: ${error.message}`);
+//     return {
+//       statusCode: error.response.status || 500,
+//       body: JSON.stringify({ error: error.message })
+//     };
+//   }
 };
