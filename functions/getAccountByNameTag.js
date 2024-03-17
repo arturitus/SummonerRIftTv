db6f265bbe34a1cb2cec635a134aab49975cb49d
arturitus <arturogranadosgimeno@gmail.com>
2024-03-17 14:13:04 +0100

    Update getAccountByNameTag.js

--- functions/getAccountByNameTag.js
+++ functions/getAccountByNameTag.js
@@ -1,25 +1,26 @@
-exports.handler = async (event, context) => {
-    const { riotServerRegion, summonerName, tagLine } = event.queryStringParameters;
+const axios = require('axios');
 
+// Function to retrieve summoner by name
+exports.handler = async (event, context) => {
+  try {
+    const { riotServerRegion, summonerName, tagLine } = event.queryStringParameters;
     const apiKey = process.env.API_KEY; // Accessing the API key from environment variable
-    // Check if required parameters are provided
-    if (!riotServerRegion || !summonerName || !tagLine) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "Missing required parameters" })
-      };
-    }
-  
-    // Construct the URL
     const url = `https://${riotServerRegion}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tagLine}?api_key=${apiKey}`;
-  
-    // Return the URL as JSON
+    
+    // Fetch data from the API
+    const response = await axios.get(url);
+
+    // Return the response to the client
     return {
       statusCode: 200,
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ url: url })
+      body: JSON.stringify(response.data)
     };
-  };
-  
+  } catch (error) {
+    // If an error occurs, return an error response
+    console.error(`Error fetching data: ${error.message}`);
+    return {
+      statusCode: error.response.status || 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
