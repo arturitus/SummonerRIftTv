5cbdf2426b2afc958a3d024f85b6bee754f710e1
arturitus <arturogranadosgimeno@gmail.com>
2024-03-17 13:29:58 +0100

    add byNameTag & puuid

---
+++ functions/getSummonerByEncryptedPUUID.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+// Function to retrieve summoner by name
+exports.handler = async (event, context) => {
+  try {
+    const { region, encryptedPUUID } = event.queryStringParameters;
+    const apiKey = process.env.API_KEY; // Accessing the API key from environment variable
+    const url = `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${encryptedPUUID}?api_key=${apiKey}`;
+    
+    // Fetch data from the API
+    const response = await axios.get(url);
+
+    // Return the response to the client
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) {
+    // If an error occurs, return an error response
+    console.error(`Error fetching data: ${error.message}`);
+    return {
+      statusCode: error.response.status || 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
