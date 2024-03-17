13327040e2d3c0a9af74e426298bfada56f28c89
arturitus <arturogranadosgimeno@gmail.com>
2024-03-17 13:11:56 +0100

    Update getSummonerByName.js

--- functions/getSummonerByName.js
+++ functions/getSummonerByName.js
@@ -1,10 +1,27 @@
 // const axios = require('axios');
 // Function to retrieve summoner by name
-exports.handler = async (region, summonerName, apiKey) => 
-{
-  // const encodedSummonerName = encodeURIComponent(summonerName);
-  return `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`;
-  // return fetchData(url);
+exports.handler = async (event, context) => {
+  const { region, summonerName, apiKey } = event.queryStringParameters;
+
+  // Check if required parameters are provided
+  if (!region || !summonerName || !apiKey) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Missing required parameters" })
+    };
+  }
+
+  // Construct the URL
+  const url = `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`;
+
+  // Return the URL as JSON
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ url: url })
+  };
 };
 
 // Helper function to fetch data from API
