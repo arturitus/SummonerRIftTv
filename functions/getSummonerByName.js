1ffce13b5c1e25acdfba7358118b77a053434801
arturitus <arturogranadosgimeno@gmail.com>
2024-03-17 13:08:53 +0100

    Update getSummonerByName.js

--- functions/getSummonerByName.js
+++ functions/getSummonerByName.js
@@ -1,23 +1,23 @@
-const axios = require('axios');
+// const axios = require('axios');
 // Function to retrieve summoner by name
 exports.handler = async (region, summonerName, apiKey) => 
 {
   // const encodedSummonerName = encodeURIComponent(summonerName);
-  return `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodedSummonerName}?api_key=${apiKey}`;
+  return `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`;
   // return fetchData(url);
 };
 
 // Helper function to fetch data from API
-const fetchData = async (url) => 
-{
-  try 
-  {
-    const response = await axios.get(url);
-    return response.data;
-  } 
-  catch (error)
-  {
-    console.error(`Error fetching data: ${error.message}`);
-    throw error;
-  }
-};
+// const fetchData = async (url) => 
+// {
+//   try 
+//   {
+//     const response = await axios.get(url);
+//     return response.data;
+//   } 
+//   catch (error)
+//   {
+//     console.error(`Error fetching data: ${error.message}`);
+//     throw error;
+//   }
+// };
